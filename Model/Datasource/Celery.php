799f5a7115a0135e0b18ee8343a78de416e71b6a
Tigran Gabrielyan <tigrangab@gmail.com>
2012-11-24 18:40:28 -0800

    Fixed formatting and added compatibility with latest Celery version

--- Model/Datasource/Celery.php
+++ Model/Datasource/Celery.php
@@ -2,35 +2,26 @@
 App::uses('AMQP', 'AMQP.Model/Datasource');
 class Celery extends AMQP {
 
-    public function publish($routingKey, $task, $args, $extras = array()) {
-        $this->connect();
-		if (!in_array($routingKey, $this->config['types']) &&
-            $this->config['types'] !== array('*')) {
-			return false;
+	public function publish($message, $routingKey, $flags = AMQP_NOPARAM, $attributes = []) {
+		$messageId = uniqid('Task_');
+
+		$task = $attributes['task'];
+		unset($attributes['task']);
+
+		$message = [
+			'id' => $messageId,
+			'task' => $task,
+			'args' => [json_encode($message)],
+			'kwargs' => (object)[],
+		];
+
+		$attributes = array_merge(['content_type' => 'application/json', 'content_encoding' => 'UTF-8'], $attributes);
+
+		if (parent::publish($message, $routingKey, $flags, $attributes)) {
+			return $messageId;
 		}
 
-        $messageId = uniqid('Task_');
-        $message = array(
-            'id' => $messageId,
-            'task' => $task,
-            'args' => array(json_encode($args)),
-            'kwargs' => (object)array(),
-        );
-        $message = json_encode(array_merge($message, $extras));
-
-        $params = array('Content-type' => 'application/json',
-            'Content-encoding' => 'UTF-8',
-            'immediate' => false,
-        );
-
-        $result = $this->_exchange->publish($message, $routingKey, 0, $params);
-        $this->disconnect();
-
-        if ($result) {
-            return $messageId;
-        }
-
-        return false;
-    }
+		return false;
+	}
 
 }
